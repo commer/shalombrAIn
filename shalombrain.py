426bc2be10179b0b425fa5928d29e1caef3ea770
commer <dotcommer00@gmail.com>
2026-03-17 08:21:25 -0400

    Update shalombrain.py

--- shalombrain.py
+++ shalombrain.py
@@ -1,3 +1,12 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
 @app.route("/")
 def home():
-    return "ShalomBrAIn is running"
+    return "ShalomBrAIn is running 🚀"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
